b2d5d93efff8c373341fefccf3020fb6f8f09773
Julien Jaumotte <julien.jaumotte@imio.be>
2019-01-24 14:04:48 +0100

    Update view with locality filter and ins street code

--- src/urban/dataimport/core/views/bestaddress_views.py
+++ src/urban/dataimport/core/views/bestaddress_views.py
@@ -1,11 +1,12 @@
 def create_bestaddress_views(import_object, locality):
     import_object.bestaddress.create_view("bestaddress_vue",
                                           """
-                                            SELECT id, street, zip, entity, commune
+                                            SELECT id, street, zip, entity, commune, key
                                             FROM public.addresses WHERE commune = '{0}';
                                           """.format(locality)
                                           )
     import_object.bestaddress.bestaddress_vue.set_index([
         'id',
+        'key',
         'street',
     ])
