50d20aa27a3d1dfa832b340044a25759adbfd85a
Julien Jaumotte <julien.jaumotte@imio.be>
2020-08-24 12:24:56 +0200

    Add html escape util method

--- src/urban/dataimport/core/utils.py
+++ src/urban/dataimport/core/utils.py
@@ -148,6 +148,20 @@ def get_filename_from_suffix_path(root_documents_path, suffix_path):
         return ntpath.basename(match_file)
 
 
+html_escape_table = {
+    "&": "&amp;",
+    '"': "&quot;",
+    "'": "&apos;",
+    ">": "&gt;",
+    "<": "&lt;",
+    }
+
+
+def html_escape(text):
+    """Produce entities within text."""
+    return "".join(html_escape_table.get(c, c) for c in text)
+
+
 class IterationError(Exception):
     pass
 
